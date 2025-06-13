ec781464aa63b391b5f7348dc80d47d26fcb8a21
Arnout112 <arnout.r.t.smit@gmail.com>
2025-06-13 11:39:42 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MobileBezorgApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
