75513946f4130060ad12e9712cee6f69905fa94f
Arnout112 <arnout.r.t.smit@gmail.com>
2025-06-24 18:01:48 +0200

    Open navigation page at user location

--- Pages/MapsPage.xaml.cs
+++ Pages/MapsPage.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.Maui.Maps;
 using Map = Microsoft.Maui.Controls.Maps.Map;
 
 namespace MobileBezorgApp;
@@ -8,6 +9,30 @@ public partial class MapsPage : ContentPage
     {
         InitializeComponent();
 
-        map.IsShowingUser = true;
+        ShowUserLocationOnMap();
+    }
+
+    private async void ShowUserLocationOnMap()
+    {
+        try
+        {
+            var location = await Geolocation.Default.GetLocationAsync();
+            if (location != null)
+            {
+                var mapSpan = MapSpan.FromCenterAndRadius(
+                    new Location(location.Latitude, location.Longitude),
+                    Distance.FromKilometers(1));
+                map.MoveToRegion(mapSpan);
+                map.IsShowingUser = true;
+            }
+            else
+            {
+                await DisplayAlert("Error", "Could not get current location.", "OK");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"Location error: {ex.Message}", "OK");
+        }
     }
 }
